cf7373f23fe8f0798d51d683f324b1561ae93cd4
Seung Wook Lee <seungwooklee94@gmail.com>
2019-09-03 01:46:35 +0900

    second day 1. Getting conditions

--- App.js
+++ App.js
@@ -1,52 +1,73 @@
-import React from 'react';
+import React from "react";
 
 //import { StyleSheet, Text, View } from 'react-native';
-import { Alert } from "react-native"
+
+// Importing all necessary components
+import {
+  Alert
+} from "react-native";
 import Loading from "./loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
-//import Weather from "./weather";
+// API key for weather API
 const API_Key = "8ff35cefdde06eb3007a2148d773df31";
 
 export default class extends React.Component {
+  // Initialize state. set isLoading as true. Cuz at this point we are loading data indeed :)
   state = {
     isLoading: true
   };
 
-  // Create new function to get weather
-  getWeather = async(latitude, longitude) => {
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_Key}`
-    ); 
-    console.log(data);
+  // Create new function to get weather. Use axios to get data from an API
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: { main: { temp }, weather } } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_Key}&units=metric`
+    );
+
+    // After getting the weather, store the temp data by using setState
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
   };
 
+  // Function to get your location.
   getLocation = async () => {
-
     try {
+      // Get permission to get user location
       Location.requestPermissionsAsync();
-      //const location = await Location.getCurrentPositionAsync();
-      const { 
-        coords: { latitude, longitude } 
+      // After permission, get lat and lon
+      const {
+        coords: {
+          latitude,
+          longitude
+        }
       } = await Location.getCurrentPositionAsync();
 
-      this.getWeather( latitude, longitude )
-      this.setState({isLoading:false});
-      
-      // send to API and get weather!
-    
+      this.getWeather(latitude, longitude);
+      this.setState({
+        isLoading: false
+      }); // Loading's completed. Set isloading: false
     } catch (error) {
-      Alert.alert("흑흑 어디에 있나요 그대", "너무너무슬퍼요!")
-    };
-
-  }
+      // When you cannot get the location O<-<
+      Alert.alert("흑흑 어디에 있나요 그대", "너무너무슬퍼요!");
+    }
+  };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const {
+      isLoading,
+      temp,
+      condition
+    } = this.state;
+    
+    // How to put temperature @ weather? We use state! Data.main.temp we need.
+    return isLoading ? ( <Loading />) :  (<Weather temp = { Math.round(temp) } condition={ condition }/> );
   }
-
-};
+}
--- weather.js
+++ weather.js
@@ -1,20 +1,42 @@
-import React, {Component} from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, Text, View } from "react-native";
 //import { LinearGradient } from 'expo';
 
-export default class Weather extends Component {
-    render() {
-        return <View style = {styles.container}></View>
-    }
+export default function Weather({ temp, condition }) {
+  console.log(temp, condition)
+  return (
+    <View style={styles.container}>
+      <Text> {temp} </Text>
+    </View>
+
+  );
 }
 
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust",
+    "Sand",
+    "Dust",
+    "Fog"
+  ]).isRequired
+};
+
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: '#55B3FF',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#55B3FF",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
